a6a966a1737eba414255ebf52e6d3ca764325ca1
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-02-09 21:03:20 +0100

    added swipable cards

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import "./Main.css";
+import SwipeCards from "../swipecards/SwipeCards";
 
 const Main = () => {
   return (
     <div className="app">
       <Header></Header>
-      <div className="main">Welcome</div>
+      <div className="main">
+        <SwipeCards />
+      </div>
       <Footer></Footer>
     </div>
   );
