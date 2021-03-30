7eac8c158ddc1e8c8467cf80dde81d00022baa55
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-03-30 10:01:05 +0200

    wip: added foursquare api

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,26 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import "./Main.css";
 import SwipeCards from "../swipecards/SwipeCards";
-// import axios from "axios";
-// import { useLoadScript } from "@react-google-maps/api";
+import axios from "axios";
 
 const Main = () => {
-  // const libraries = ["places"];
-  // // url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Poissy&key=";
-  // const { isLoaded, loadError } = useLoadScript({
-  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-  //   libraries,
-  // });
+  let places = [];
 
-  // if (loadError) return "Error loading maps";
-  // if (!isLoaded) return "Loading maps";
+  useEffect(() => {
+    getLocation();
+  }, []);
 
   const getLocation = () => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
+        console.log(position.coords);
+        let url = `https://api.foursquare.com/v2/venues/search?client_id=${process.env.REACT_APP_FOURSQUARE_CLIENT_ID}&client_secret=${process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET}&v=20180323&limit=10&ll=${position.coords.latitude},${position.coords.longitude}&query=food`;
+        axios
+          .get(url)
+          .then((res) => {
+            res.data.response.venues.forEach((venue) => {
+              console.log(venue);
+              places.push(venue);
+              // REQUEST TO FETCH VENUE PHOTO
+              //   axios
+              //     .get(
+              //       ` https://api.foursquare.com/v2/venues/${venue.id}/photos?client_id=${process.env.REACT_APP_FOURSQUARE_CLIENT_ID}&client_secret=${process.env.REACT_APP_FOURSQUARE_CLIENT_SECRET}&v=20180323`
+              //     )
+              //     .then((result) => {
+              //       result.data.response.photos.items.map((item) => {
+              //         let suffix = item.suffix;
+              //         let prefix = item.prefix;
+              //         const photoUrl = suffix + prefix;
+              //         console.log(photoUrl);
+              //       });
+              //     });
+            });
+          })
+          .catch((e) => console.log("Error:", e));
       },
       () => null
     );
@@ -33,7 +51,7 @@ const Main = () => {
         <button className="locate" onClick={getLocation}>
           Locate me
         </button>
-        <SwipeCards />
+        <SwipeCards places={places} />
       </div>
       <Footer></Footer>
     </div>
--- src/components/swipecards/SwipeCards.js
+++ src/components/swipecards/SwipeCards.js
@@ -2,40 +2,56 @@ import "./SwipeCards.css";
 import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 
-const SwipeCards = () => {
-  const [data, setData] = useState([
-    {
-      name: "resto1",
-      url:
-        "https://images.pexels.com/photos/1484516/pexels-photo-1484516.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-    },
-    {
-      name: "resto2",
-      url:
-        "https://images.pexels.com/photos/735869/pexels-photo-735869.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    },
-  ]);
+const SwipeCards = ({ places }) => {
+  // const [data, setData] = useState([
+  //   {
+  //     name: "resto1",
+  //     url:
+  //       "https://images.pexels.com/photos/1484516/pexels-photo-1484516.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+  //   },
+  //   {
+  //     name: "resto2",
+  //     url:
+  //       "https://images.pexels.com/photos/735869/pexels-photo-735869.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  //   },
+  // ]);
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (data) {
+      setData(places);
+      setLoading(false);
+    }
+  }, [data]);
 
   return (
     <div>
-      <div className="tinderCards__cardsContainer">
-        {data.map((data) => (
-          <TinderCard
-            className="swipe"
-            key={data.name}
-            preventSwipe={["up", "down"]}
-          >
-            <div
-              className="card"
-              style={{
-                backgroundImage: `url(${data.url})`,
-              }}
+      {loading ? (
+        <div>I am loading</div>
+      ) : (
+        <div className="tinderCards__cardsContainer">
+          {data.map((data) => (
+            <TinderCard
+              className="swipe"
+              key={data.id}
+              preventSwipe={["up", "down"]}
             >
-              <h3>{data.name}</h3>
-            </div>
-          </TinderCard>
-        ))}
-      </div>
+              <div
+                className="card"
+                style={{
+                  // backgroundImage: `url(${data.url})`,
+                  backgroundColor: "black",
+                }}
+              >
+                <p>Test</p>
+                <h3>{data.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
